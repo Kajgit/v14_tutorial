3590ce0f94cd9409a33064c2b114d1c6e762e50f
Kaj <kajvdm@gmail.com>
2022-11-30 21:05:43 +0100

    Update messageCreate.js

--- Events/Messages/messageCreate.js
+++ Events/Messages/messageCreate.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder } = require("discord.js");
-const Levels = require("discord.js-leveling");
 
 module.exports = {
     name: "messageCreate",
@@ -19,25 +18,6 @@ module.exports = {
         if (foundInText) {
             message.delete();
             message.channel.send(`${message.author}, your message was flagged for swearing!`);
-        }
-
-        //xp system
-        if (message.content.length < 3) return; // sending messages like 'ok' or 'no' does not add xp
-
-        const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; //min 1, max 30
-        const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
-
-        if (hasLeveledUp) {
-            const user = await Levels.fetch(message.author.id, message.guild.id);
-
-            const levelEmbed = new EmbedBuilder()
-                .setTitle("New Level!")
-                .setDescription(`**GG** ${message.author}, you just leveled up to level **${user.level + 1}**! 🥳`)
-                .setColor("Random")
-                .setTimestamp();
-
-            const sendEmbed = await message.channel.send({ embeds: [levelEmbed] });
-            sendEmbed.react('🥳');
         }
     }
 }
