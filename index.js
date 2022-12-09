bc98af00a315e8d877aecdb65ededf0d8815527e
Kaj <kajvdm@gmail.com>
2022-12-09 17:57:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const {
 const YoutubePoster = require("discord-youtube");
 const logs = require("discord-logs");
 
-const { DisTube } = require("distube");
-const { SpotifyPlugin } = require("@distube/spotify");
-
 const { handleLogs } = require("./Handlers/handleLogs");
 const { loadEvents } = require("./Handlers/eventHandler");
 const { loadCommands } = require("./Handlers/commandHandler");
@@ -23,12 +20,6 @@ logs(client, {
   debug: true
 });
 
-client.distube = new DisTube(client, {
-  emitNewSongOnly: true,
-  leaveOnFinish: true, // you can change this to your needs
-  emitAddSongWhenCreatingQueue: false,
-  plugins: [new SpotifyPlugin()]
-});
 client.ytp = new YoutubePoster(client, {
   loop_delay_in_min: 1
 });
