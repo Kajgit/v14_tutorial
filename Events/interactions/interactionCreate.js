fc994db482ec473f133eae1b00dc06c6dc3f662f
Kaj <kajvdm@gmail.com>
2022-12-09 18:02:54 +0100

    Update interactionCreate.js

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -22,26 +22,6 @@ module.exports = {
           })
         );
       }
-    } else if (interaction.isSelectMenu()) {
-      if (customId == "reaction-roles") {
-        for (let i = 0; i < values.length; i++) {
-          const roleId = values[i];
-
-          const role = guild.roles.cache.get(roleId);
-          const hasRole = member.roles.cache.has(roleId);
-
-          switch (hasRole) {
-            case true:
-              member.roles.remove(roleId);
-              break;
-            case false:
-              member.roles.add(roleId);
-              break;
-          }
-        }
-
-        interaction.reply({ content: "Roles updated.", ephemeral: true });
-      }
     } else {
       return;
     }
