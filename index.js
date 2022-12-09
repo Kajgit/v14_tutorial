827392159c1933979628f0b4e57d8e78b7b55241
Kaj <kajvdm@gmail.com>
2022-12-09 14:44:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const {
   Partials,
   Collection,
 } = require("discord.js");
+const YoutubePoster = require("discord-youtube");
 const logs = require("discord-logs");
+
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
 
 const { handleLogs } = require("./Handlers/handleLogs");
 const { loadEvents } = require("./Handlers/eventHandler");
@@ -19,8 +23,19 @@ logs(client, {
   debug: true
 });
 
+client.distube = new DisTube(client, {
+  emitNewSongOnly: true,
+  leaveOnFinish: true, // you can change this to your needs
+  emitAddSongWhenCreatingQueue: false,
+  plugins: [new SpotifyPlugin()]
+});
+client.ytp = new YoutubePoster(client, {
+  loop_delay_in_min: 1
+});
 client.commands = new Collection();
 client.config = require("./config.json");
+
+module.exports = client;
 
 client.login(client.config.token).then(() => {
   handleLogs(client);
